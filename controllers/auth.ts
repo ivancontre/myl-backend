b5f6901ed68eaf6a9d169204759ba2bf733db810
Ivan Contreras <ivanc.contre@gmail.com>
2021-12-05 17:31:43 -0300

    ADD: Se cambia nombre variable de ambiente

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -78,7 +78,7 @@ export const register = async (req: Request, res: Response) => {
 
         await user.save();
 
-        if (process.env.CONFIRM_REGISTER === 'false') {
+        if (process.env.STATUS_REGISTER === 'false') {
             return res.status(401).json({
                 msg: `El usuario se registró correctamente, pero no se encuentra activo. Hable con el administrador`
             });
